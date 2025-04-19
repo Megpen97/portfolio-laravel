a23b3557086be5c47e32edba5e170b91bcdf9c7a
megpen97 <megpen97@yahoo.com>
2025-04-19 15:08:48 -0400

    autoload

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit228885af06a1619ec131f532cc07ccdb
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit228885af06a1619ec131f532cc07ccdb', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit228885af06a1619ec131f532cc07ccdb', 'loadClassLoader'));
